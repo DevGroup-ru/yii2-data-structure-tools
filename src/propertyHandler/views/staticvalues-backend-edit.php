9187d528289f5912c7c30afcade16e27f3a24cbf
dmitriy <hector.tmb@gmail.com>
2016-01-28 11:36:06 +0300

    sort in select2

--- src/propertyHandler/views/staticvalues-backend-edit.php
+++ src/propertyHandler/views/staticvalues-backend-edit.php
@@ -5,20 +5,41 @@
  * @var yii\web\View $this
  */
 
+use DevGroup\DataStructure\assets\Select2SortableBundle;
+use yii\helpers\Html;
+use yii\web\View;
 use yii\widgets\ActiveForm;
 use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
+
+$dataQuery = $property->getStaticValues();
+
+if ((int)$property->allow_multiple_values === 1 && !empty($model->{$property->key})) {
+    $dataQuery->orderBy([
+        new \yii\db\Expression('FIELD(id, '.implode(',', $model->{$property->key}).'), sort_order')
+    ]);
+}
 
 echo (new ActiveForm())
     ->field($model, $property->key)
     ->widget(
         Select2::className(),
         [
-            'data' => ArrayHelper::map($property->staticValues, 'id', 'name'),
+            'data' => ArrayHelper::map($dataQuery->all(), 'id', 'name'),
             'options' => ['placeholder' => 'Select a value ...'],
             'pluginOptions' => [
                 'allowClear' => true,
-                'multiple' => $property->allow_multiple_values
+                'multiple' => (int)$property->allow_multiple_values === 1
             ],
         ]
     );
+
+if ((int)$property->allow_multiple_values === 1) {
+    Select2SortableBundle::register($this);
+    $id = \yii\helpers\Html::getInputId($model, $property->key);
+    $this->registerJs(<<<js
+    $('#$id').select2Sortable();
+js
+    );
+
+}
